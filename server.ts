f0e99297162edea2c9c1586ffc099b48665ecda6
Giuseppe Buoncore <135731752+GiuseppeBuoncore@users.noreply.github.com>
2024-05-17 10:49:08 -0700

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+app.use(morgan("dev"))
+
+type Planet = {
+  id: number,
+  name: string,
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+  {
+    id: 1,
+    name: "Earth",
+  },
+  {
+    id: 2,
+    name: "Mars",
+  },
+];
+
+app.get('/api/planets', (req, res) => {
+  res.status(200).json(planets)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
